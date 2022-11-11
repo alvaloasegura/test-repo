9abca62e385f955940889c47eb6e832c13c2df85
alvaloasegura <alva_loa@hotmail.com>
2022-11-10 21:45:06 -0400

    test code

---
+++ hola.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * It adds two numbers and subtracts the product of the two numbers
+ * 
+ * @param a The first number
+ * @param b the number of bits in the output
+ * 
+ * @return a + b - a*b
+ */
+int add(int a, int b)
+{
+    return a + b -a*b;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int c = 4;
+    // printf("Enter: ");
+    scanf("%d", &c);
+    printf("$Resultado: %d", add(4, 5));
+    return 0;
+}
